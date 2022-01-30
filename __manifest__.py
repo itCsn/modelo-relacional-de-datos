f6fc04a80cc5b1e58823e048991f8d1300d16a2b
Italo <italo.casanova.d@uni.pe>
2022-01-30 04:50:15 -0500

    vista, sub menus, permisos de usuario

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,9 @@
     'sumary':"""""",
     'data':[
      'views/menu_item_views.xml'
-     'views/student_views.xml'
-     'views/subject_views.xml'
-     'views/teacher_views.xml'
+     'views/school_student_views.xml'
+     'views/school_subject_views.xml'
+     'views/school_teacher_views.xml'
+     'security/security.xml'
     ]
 }
