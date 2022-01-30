b6f029f11c7e1780785fa1a10bfc5ea5b57265e0
Italo <italo.casanova.d@uni.pe>
2022-01-30 15:18:02 -0500

    correcion error vistas

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,8 @@
 {
     "name": "modelo",
-    'sumary':"""""",
+    'sumary':""" 
+    modelo relacional de datos para el rubro educacion-colegio
+    """,
     'data':[
      'views/menu_item_views.xml'
      'views/school_student_views.xml'
