53e511c66107a77d5ba2629b3e84f726a8ef92fc
Italo <italo.casanova.d@uni.pe>
2022-01-30 01:47:09 -0500

    age computed

--- __manifest__.py
+++ __manifest__.py
@@ -1,3 +1,10 @@
 {
-    "name": "modelo"
+    "name": "modelo",
+    'sumary':"""""",
+    'data':[
+     'views/menu_item_views.xml'
+     'views/student_views.xml'
+     'views/subject_views.xml'
+     'views/teacher_views.xml'
+    ]
 }
